29c1120025bfdc6fd3172655bbcc6147d36e49b5
guilhermeicarofr <guilhermeicarofr@gmail.com>
2023-05-17 12:57:41 -0300

    Adicionado: criado exceptionHandler para lidar com os throws de exceptions personalizadas com seus devidos status code e body de resposta nas requisições sem sucesso

---
+++ colaboradores/src/main/java/api/colaboradores/controller/ExceptionHandlerController.java
@@ -0,0 +1,44 @@
+package api.colaboradores.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import api.colaboradores.exceptions.ColaboradorException;
+import api.colaboradores.exceptions.CpfException;
+import api.colaboradores.exceptions.HierarquiaException;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+  
+  @ExceptionHandler(CpfException.class)
+  public ResponseEntity<String> handleCpfException(CpfException e) {
+    return ResponseEntity.status(CpfException.STATUS).body(CpfException.MESSAGE);
+  }
+
+  @ExceptionHandler(ColaboradorException.class)
+  public ResponseEntity<String> handleColaboradorException(ColaboradorException e) {
+    return ResponseEntity.status(ColaboradorException.STATUS).body(ColaboradorException.MESSAGE + e.messageId);
+  }
+
+  @ExceptionHandler(HierarquiaException.class)
+  public ResponseEntity<String> handleHierarquiaException(HierarquiaException e) {
+    return ResponseEntity.status(HierarquiaException.STATUS).body(e.message);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<List<String>> handleValidationException(MethodArgumentNotValidException e) {
+    List<String> validationErrors = new ArrayList<>();
+    List<ObjectError> errors = e.getBindingResult().getAllErrors();
+    for(int i=0; i<errors.size(); i++) {
+      validationErrors.add(errors.get(i).getDefaultMessage());
+    }
+
+    return ResponseEntity.badRequest().body(validationErrors);
+  }
+}
